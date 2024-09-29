3a4b64fc2d2fdc3e954588099a2fd75d498eb341
Gustavo Torres <gustatorres132@gmail.com>
2024-09-29 16:52:43 -0300

    Auto stash before rebase of "refs/heads/GustavoT"

---
+++ Medallia-login/src/main/java/com/example/Medallia_login/Servicio/ServicioCuenta.java
@@ -0,0 +1,27 @@
+package com.example.Medallia_login.Servicio;
+
+import com.example.Medallia_login.Modelos.Cuenta;
+import com.example.Medallia_login.Repositories.RepositorioCuenta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioCuenta {
+        @Autowired
+        private RepositorioCuenta repositoriocuenta;
+
+
+
+
+    public boolean login(String correo, String contrasena) {
+        Cuenta cuenta = repositoriocuenta.findByCorreo(correo);
+        if (cuenta != null) {
+            return (contrasena, cuenta.getContrasena());
+        }
+        return false;
+    }
+}
+
+
+}
