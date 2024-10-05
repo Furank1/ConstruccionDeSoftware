cb8c9c26e18e693e7de0bb9a0f742f41c94cd023
BorisMejias <128553727+BorisMejiasII@users.noreply.github.com>
2024-10-05 20:50:33 -0300

    Commit login
    +Agregando funcionalidades al login

--- medallia-frontend/src/app/login/login.component.ts
+++ medallia-frontend/src/app/login/login.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [FormsModule]
+  imports: [CommonModule, FormsModule, HttpClientModule]  // Asegúrate de importar HttpClientModule aquí
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit() {
-    console.log("Formulario enviado");
-    // Aquí puedes agregar lógica adicional si es necesario
+    const loginData = {
+      email: this.email,
+      password: this.password
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    this.http.post<any>('http://localhost:8080/api/auth/login', loginData, { headers })
+      .subscribe(
+        response => {
+          console.log('Login exitoso');
+          this.router.navigate(['/registro']);
+        },
+        error => {
+          console.error('Error de login', error);
+          this.errorMessage = 'Credenciales incorrectas';
+        }
+      );
   }
+
   goToRegister() {
     this.router.navigate(['/registro']); // Redirige a la ruta de creación de cuenta
   }
