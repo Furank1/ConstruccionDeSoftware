9ee6490cd5645f8319fc66d5c5daccca3713fe01
novoa123 <jnovoa20@alumnos.utalca.cl>
2024-10-05 17:36:25 -0300

    lolardium
    a

--- constru1/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { TestComponent } from './test.component';
-
-export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'test', component: TestComponent }
-];
--- constru1_backend/src/main/java/com/example/Constru1/domain/AccountDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.Constru1.domain;
-
-public class AccountDTO {
-    public String email;
-    public String password;
-
-    public String getEmail(){return email;}
-    public String getPassword(){return password;}
-}
