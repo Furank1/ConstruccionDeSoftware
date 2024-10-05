f05c8a34b33b8734dcec6091dd01fd27b2b1c54d
David Ochoa <dochoa21@alumnos.utalca.cl>
2024-10-05 17:17:04 -0300

    Limpiando branch, para traer cambios del main

--- frontendDAVID/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app.routes';
-import {importProvidersFrom} from "@angular/core";
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)]
-}).catch(err => console.error(err));
