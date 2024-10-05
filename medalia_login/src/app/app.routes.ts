361e349262b4e779fab2ee0dd12395e3fcda65cd
Gustavo Torres <gustatorres132@gmail.com>
2024-10-05 16:53:45 -0300

    borrando archivos innecesarios
    se duplico el back y el front

--- medalia_login/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; // Asegúrate de que la ruta sea correcta
-
-export const routes = [
-    { path: '', component: LoginComponent }, // Ruta para el componente de login
-  ];
