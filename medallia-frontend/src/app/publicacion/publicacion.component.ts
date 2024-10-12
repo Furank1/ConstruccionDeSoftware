f91035d8e43677be18c92fedd362ac2a6905b57c
Catalina Miño <cmino21@alumnos.utalca.cl>
2024-10-12 12:45:04 -0300

    se muestran aplausos en publicaciones
    se muestran el numero de aplausos en el botón de aplaudir

--- medallia-frontend/src/app/publicacion/publicacion.component.ts
+++ medallia-frontend/src/app/publicacion/publicacion.component.ts
@@ -1,20 +1,50 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-publicacion',
   templateUrl: './publicacion.component.html',
   standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrls: ['./publicacion.component.css']
 })
 export class PublicacionComponent {
-  @Input() imageUrl!: string;
-  @Input() titulo!: string;
-  @Input() descripcion!: string;
-  @Input() categoria!: string;
-  @Input() aplausos!: number;  // Recibe el número de aplausos desde el componente padre
 
+  @Input() imagen!: string;
+  @Input() usuarioId!: string;
+  @Input() descripcion!: string;
+  @Input() fecha!: Date;
+  @Input() aplausos!: number;  // Recibe el número de aplausos desde el componente padre
+  posts: any[] = [];
   // Función que incrementa el número de aplausos
+  @Output() aplauso = new EventEmitter<void>();
   onAplaudir() {
     this.aplausos++;
+  }
+  ngOnInit(): void {
+    this.cargarPublicaciones();
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  cargarPublicaciones(): void {
+    this.http.get<any[]>('http://localhost:8080/publicaciones/get')
+      .subscribe(
+        (data: any[]) => {
+          console.log('Publicaciones cargadas', data);
+
+          this.posts = data.map(post => ({
+            ...post,
+            aplaudido: false
+          }));
+        },
+        (error: any) => {
+          console.error('Error al cargar publicaciones', error);
+        }
+      );
   }
 }
