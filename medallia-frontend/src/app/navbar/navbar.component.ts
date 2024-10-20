8c0ad5a0b85c45e30f53c840f5f7728e40f1a17f
David Ochoa <dochoa21@alumnos.utalca.cl>
2024-10-20 00:46:09 -0300

    Nombre de usuario
    - En cada componente se visualiza cual es el usuario logueado, anteriormente no se sabía quien estaba logueado

--- medallia-frontend/src/app/navbar/navbar.component.ts
+++ medallia-frontend/src/app/navbar/navbar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  standalone:true,
+  standalone: true,
+  imports: [NgClass, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(private router: Router) {}
+  loggedInUser: string | null = null;
+
+  constructor(private router: Router) {
+    // nombredeusuariologueao
+    this.loggedInUser = localStorage.getItem('loggedInUser');
+  }
 
   irAProfile() {
     this.router.navigate(['/profile']);
