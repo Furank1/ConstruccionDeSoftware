d02bcea5a04ba79ebfe898a713941b9a66ed250a
Gustavo Torres <gustatorres132@gmail.com>
2024-10-26 19:25:34 -0300

    Se envian todos los reportes al front end
    terminado el endpoint que permite mandar todos los reportes al front end para ser mostrados

--- Medallia-login/src/main/java/com/example/Medallia_login/Servicio/ServicioReporte.java
+++ Medallia-login/src/main/java/com/example/Medallia_login/Servicio/ServicioReporte.java
@@ -1,7 +1,9 @@
 package com.example.Medallia_login.Servicio;
 
+import com.example.Medallia_login.Modelos.Cuenta;
 import com.example.Medallia_login.Modelos.Publicacion;
 import com.example.Medallia_login.Modelos.Reporte;
+import com.example.Medallia_login.Repositories.RepositorioCuenta;
 import com.example.Medallia_login.Repositories.RepositorioPublicaciones;
 import com.example.Medallia_login.Repositories.RepositorioReportes;
 import org.bson.types.ObjectId;
@@ -14,6 +16,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServicioReporte {
@@ -22,6 +25,8 @@ public class ServicioReporte {
     private RepositorioReportes repositorioReportes;
     @Autowired
     private RepositorioPublicaciones repositorioPublicaciones;
+    @Autowired
+    private RepositorioCuenta repositorioCuenta;
 
     @Transactional
     public Reporte crearReporte(ObjectId objIdUsuario, ObjectId objIdPublicacion, Instant fecha, String descripcion){
@@ -33,12 +38,28 @@ public class ServicioReporte {
         repositorioReportes.save(reporte);
         return reporte;
     }
-    public List<HashMap<String,String>> devolverpublicaciones(){
-         List <Reporte> reportes = repositorioReportes.findAll();
-         for (int i=0;i<reportes.size();i++){
-             HashMap<String,String> aux1 = new HashMap<>();
 
-         }
-         return null;
+
+    public List<HashMap<String,String>> devolverreportes(){
+         List <Reporte> reportes = repositorioReportes.findAll();
+         List<HashMap<String,String>> reporteList = new ArrayList<>();
+        for (Reporte reporte : reportes) {
+            HashMap<String, String> aux1 = new HashMap<>();
+            String id = reporte.getUsuarioId().toString();
+            Optional<Cuenta> cuenta = repositorioCuenta.findById(id);
+            if (cuenta.isPresent()) {
+                aux1.put("nombreUsuario", cuenta.get().getEmail());
+            }
+            aux1.put("fecha", reporte.getFecha().toString());
+            aux1.put("descripcion", reporte.getDescripcion());
+            Publicacion publicacion = repositorioPublicaciones.findById(reporte.getPublicacionId()).orElse(null);
+            if (publicacion != null) {
+                aux1.put("DescripcionPublicacion", publicacion.getDescripcion());
+                aux1.put("imagen", publicacion.getImagen());
+            }
+            reporteList.add(aux1);
+
+        }
+         return reporteList;
     }
 }
