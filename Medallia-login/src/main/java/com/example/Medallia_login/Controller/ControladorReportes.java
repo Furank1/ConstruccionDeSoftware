2631fd842d2bc125c012b34cedd82e323989c28a
Gustavo Torres <gustatorres132@gmail.com>
2024-10-26 15:15:51 -0300

    mapeo de restcontroller para reportes

--- Medallia-login/src/main/java/com/example/Medallia_login/Controller/ControladorReportes.java
+++ Medallia-login/src/main/java/com/example/Medallia_login/Controller/ControladorReportes.java
@@ -1,4 +1,16 @@
 package com.example.Medallia_login.Controller;
 
+import com.example.Medallia_login.Servicio.ServicioReporte;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reporte")
 public class ControladorReportes {
+
+    @Autowired
+    private ServicioReporte servicioReporte;
+
+
 }
