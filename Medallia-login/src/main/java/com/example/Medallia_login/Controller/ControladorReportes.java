2b32f66359fec22bce31120177c3a8b124763a36
novoa123 <jnovoa20@alumnos.utalca.cl>
2024-10-26 18:10:11 -0300

    Crear reportes

--- Medallia-login/src/main/java/com/example/Medallia_login/Controller/ControladorReportes.java
+++ Medallia-login/src/main/java/com/example/Medallia_login/Controller/ControladorReportes.java
@@ -1,9 +1,12 @@
 package com.example.Medallia_login.Controller;
 
+import com.example.Medallia_login.Dominio.ReporteDTO;
+import com.example.Medallia_login.Modelos.Reporte;
 import com.example.Medallia_login.Servicio.ServicioReporte;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reporte")
@@ -11,6 +14,16 @@ public class ControladorReportes {
 
     @Autowired
     private ServicioReporte servicioReporte;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/register")
+    public ResponseEntity<?> crearReporte(@RequestBody ReporteDTO reporteDTO){
+        System.out.println("Llega ReporteDTO");
+        ObjectId objIdUsuario = new ObjectId(reporteDTO.getUsuarioId());
+        ObjectId objIdPublicacion = new ObjectId(reporteDTO.getPublicacionId());
+        Reporte reporteGuardado = servicioReporte.crearReporte(objIdUsuario, objIdPublicacion, reporteDTO.getFecha(), reporteDTO.getDescripcion());
+        return ResponseEntity.ok(reporteGuardado);
+    }
 
 
 }
--- Medallia-login/src/main/java/com/example/Medallia_login/Servicio/ServicioReporte.java
+++ Medallia-login/src/main/java/com/example/Medallia_login/Servicio/ServicioReporte.java
@@ -1,8 +1,13 @@
 package com.example.Medallia_login.Servicio;
 
+import com.example.Medallia_login.Modelos.Reporte;
 import com.example.Medallia_login.Repositories.RepositorioReportes;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
 
 @Service
 public class ServicioReporte {
@@ -10,5 +15,14 @@ public class ServicioReporte {
     @Autowired
     private RepositorioReportes repositorioReportes;
 
-
+    @Transactional
+    public Reporte crearReporte(ObjectId objIdUsuario, ObjectId objIdPublicacion, Instant fecha, String descripcion){
+        Reporte reporte = new Reporte();
+        reporte.setUsuarioId(objIdUsuario);
+        reporte.setPublicacionId(objIdPublicacion);
+        reporte.setFecha(fecha);
+        reporte.setDescripcion(descripcion);
+        repositorioReportes.save(reporte);
+        return reporte;
+    }
 }
